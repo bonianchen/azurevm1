20eded0e26ba2cdc4004027df13fd8b70ad04de2
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-03-31 20:44:35 +0800

    Update westus3_delete.sh

--- westus3_delete.sh
+++ westus3_delete.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-SPOT_RES=202303vm_res_group
+SPOT_RES=202303vm_wus3_res_group
 
 az disk delete --resource-group ${SPOT_RES} --name `az disk list --resource-group ${SPOT_RES} | grep '"name"' | head -1 | cut -d\" -f4` -y
+az network nic delete --resource-group ${SPOT_RES} --name `az network nic list --resource-group ${SPOT_RES} | grep '"name"' | tail -1 | cut -d\" -f4`
