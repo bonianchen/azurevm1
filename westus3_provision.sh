7fda3791a6ac3c706c46fdabba3e00d7ab3b3edb
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-03-31 11:56:11 +0800

    Update westus3_provision.sh

--- westus3_provision.sh
+++ westus3_provision.sh
@@ -3,4 +3,4 @@
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
-sudo apt-get update; sudo apt-get install -y tailscale; sudo tailscale up
+sudo apt-get update; sudo apt-get install -y tailscale mosh; sudo tailscale up
