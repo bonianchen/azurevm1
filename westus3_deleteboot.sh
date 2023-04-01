5e9f0e7831038074831fb9beb42c1c5519c76a1d
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-04-01 11:08:35 +0800

    Update westus3_deleteboot.sh

--- westus3_deleteboot.sh
+++ westus3_deleteboot.sh
@@ -5,3 +5,4 @@ SPOT_RES=202303vm_wus3_res_group
 az vm delete --resource-group ${SPOT_RES} --name `az vm list --resource-group ${SPOT_RES} | grep boot | grep '"name"' | head -1 | cut -d\" -f4` -y
 az disk delete --resource-group ${SPOT_RES} --name `az disk list --resource-group ${SPOT_RES} | grep boot | grep '"name"' | head -1 | cut -d\" -f4` -y
 az network nic delete --resource-group ${SPOT_RES} --name `az network nic list --resource-group ${SPOT_RES} | grep boot | grep '"name"' | tail -1 | cut -d\" -f4`
+az network public-ip delete --resource-group ${SPOT_RES} --name `az network public-ip list --resource-group ${SPOT_RES} | grep '"name"' | head -1 | cut -d\" -f4`
