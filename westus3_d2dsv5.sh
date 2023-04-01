df6358d9bbd411fbb206dce930d346fe053132b9
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-04-01 08:19:19 +0000

    Update westus3_d2dsv5.sh

--- westus3_d2dsv5.sh
+++ westus3_d2dsv5.sh
@@ -8,11 +8,11 @@ az group create --name ${SPOT_RES} --location ${SPOT_LOC}
 SPOT_VNET=202303vm_net
 SPOT_NSG=202303vm_net_nsg
 
-az network vnet create --name ${SPOT_VNET} --resource-group ${SPOT_RES} --location ${SPOT_LOC}
+az network vnet create --name ${SPOT_VNET} --resource-group ${SPOT_RES} --location ${SPOT_LOC} &
 az network nsg create --name ${SPOT_NSG} --resource-group ${SPOT_RES} --location ${SPOT_LOC}
 
 az network nsg rule create --name ${SPOT_NSG}_rule1 --resource-group ${SPOT_RES} --nsg-name ${SPOT_NSG} --priority 1000 --destination-address-prefixes '*' --destination-port 22 --access Allow --protocol Tcp
-az network nsg rule create --name ${SPOT_NSG}_rule2 --resource-group ${SPOT_RES} --nsg-name ${SPOT_NSG} --priority 1001 --destination-address-prefixes '*' --destination-port-ranges 60000-61000 --access Allow --protocol Udp
+az network nsg rule create --name ${SPOT_NSG}_rule2 --resource-group ${SPOT_RES} --nsg-name ${SPOT_NSG} --priority 1001 --destination-address-prefixes '*' --destination-port-ranges 60000-61000 --access Allow --protocol Udp &
 
 #  --public-ip-sku Basic \
 az vm create \
@@ -50,4 +50,3 @@ IPA1=`grep publicIp log_A1_v2 | cut -d\" -f4`
 scp -o StrictHostKeyChecking=accept-new -i CRED.pem CRED.pem azureuser@${IPA1}:/home/azureuser
 scp -i CRED.pem azurevm1/westus3_provision.sh azureuser@${IPA1}:/home/azureuser
 ssh -i CRED.pem azureuser@${IPA1} "scp -o StrictHostKeyChecking=accept-new -i CRED.pem westus3_provision.sh azureuser@${IPD2}:/home/azureuser; ssh -i CRED.pem azureuser@${IPD2} 'source westus3_provision.sh'"
-
