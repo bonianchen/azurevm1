246f182fa394e3426ba0951d2d5eefa475fdcdbd
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-04-02 08:16:20 +0800

    Create centralin_deleteboot.sh

---
+++ centralin_deleteboot.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SPOT_RES=202303vm_cin_res_group
+
+az vm delete --resource-group ${SPOT_RES} --name `az vm list --resource-group ${SPOT_RES} | grep boot | grep '"name"' | head -1 | cut -d\" -f4` -y
+az disk delete --resource-group ${SPOT_RES} --name `az disk list --resource-group ${SPOT_RES} | grep boot | grep '"name"' | head -1 | cut -d\" -f4` -y
+az network nic delete --resource-group ${SPOT_RES} --name `az network nic list --resource-group ${SPOT_RES} | grep boot | grep '"name"' | tail -1 | cut -d\" -f4`
+az network public-ip delete --resource-group ${SPOT_RES} --name `az network public-ip list --resource-group ${SPOT_RES} | grep '"name"' | head -1 | cut -d\" -f4`
