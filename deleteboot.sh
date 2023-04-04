48e537ebedd59309052cfd6b35715a24d6365114
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-04-04 03:05:29 +0000

    Update and rename westus3_deleteboot.sh to deleteboot.sh

--- deleteboot.sh
+++ deleteboot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SPOT_RES=202303vm_wus3_res_group
+SPOT_RES=`grep '"resourceGroup"' log_A1_v2 | cut -d\" -f4`
 
 az vm delete --resource-group ${SPOT_RES} --name `az vm list --resource-group ${SPOT_RES} | grep boot | grep '"name"' | head -1 | cut -d\" -f4` -y
 az disk delete --resource-group ${SPOT_RES} --name `az disk list --resource-group ${SPOT_RES} | grep boot | grep '"name"' | head -1 | cut -d\" -f4` -y
