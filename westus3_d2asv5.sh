e47f7b038fd67bdd9e877cf4f522c1c55b12f055
Bonian Chen <32609525+bonianchen@users.noreply.github.com>
2023-04-04 02:52:28 +0000

    Update westus3_d2asv5.sh

--- westus3_d2asv5.sh
+++ westus3_d2asv5.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-SPOT_NAME=202303vm
-SPOT_RES=202303vm_wus3_res_group
-SPOT_LOC=westus3
-SPOT_KEY=202303vm_sshkey
+SPOT_NAME=`grep ^SPOT_NAME $1 | cut -d= -f2`
+SPOT_RES=`grep ^SPOT_RES $1 | cut -d= -f2`
+SPOT_LOC=`grep ^SPOT_LOC $1 | cut -d= -f2`
+SPOT_KEY=`grep ^SPOT_KEY $1 | cut -d= -f2`
+SPOT_TYPE=`grep ^SPOT_TYPE $1 | cut -d= -f2`
 
 az group create --name ${SPOT_RES} --location ${SPOT_LOC}
 
@@ -13,14 +14,14 @@ SPOT_NSG=${SPOT_NAME}_net_nsg
 az network vnet create --name ${SPOT_VNET} --resource-group ${SPOT_RES} --location ${SPOT_LOC} &
 az network nsg create --name ${SPOT_NSG} --resource-group ${SPOT_RES} --location ${SPOT_LOC}
 
-az network nsg rule create --name ${SPOT_NSG}_rule1 --resource-group ${SPOT_RES} --nsg-name ${SPOT_NSG} --priority 1000 --destination-address-prefixes '*' --destination-port 22 --access Allow --protocol Tcp
+az network nsg rule create --name ${SPOT_NSG}_rule1 --resource-group ${SPOT_RES} --nsg-name ${SPOT_NSG} --priority 1000 --destination-address-prefixes '*' --destination-port 22 --access Allow --protocol Tcp &
 az network nsg rule create --name ${SPOT_NSG}_rule2 --resource-group ${SPOT_RES} --nsg-name ${SPOT_NSG} --priority 1001 --destination-address-prefixes '*' --destination-port-ranges 60000-61000 --access Allow --protocol Udp &
 
 #  --public-ip-sku Basic \
 az vm create \
   --resource-group ${SPOT_RES} \
   --location ${SPOT_LOC} \
-  --size Standard_D2as_v5 \
+  --size ${SPOT_TYPE} \
   --name ${SPOT_NAME} \
   --image Debian:debian-11:11:latest \
   --vnet-name ${SPOT_VNET} \
@@ -29,7 +30,10 @@ az vm create \
   --admin-username azureuser \
   --priority Spot \
   --eviction-policy Deallocate \
-  --ssh-key-name ${SPOT_KEY} &> log_D2as_v5
+  --ssh-key-name ${SPOT_KEY} &> log_vm
+
+IPVM=`grep privateIp log_vm | cut -d\" -f4`
+echo "${SPOT_NAME} created: ${IPVM}"
 
 az vm create \
   --resource-group ${SPOT_RES} \
@@ -46,9 +50,8 @@ az vm create \
   --eviction-policy Deallocate \
   --ssh-key-name ${SPOT_KEY} &> log_A1_v2
 
-IPD2=`grep privateIp log_D2as_v5 | cut -d\" -f4`
 IPA1=`grep publicIp log_A1_v2 | cut -d\" -f4`
 
 scp -o StrictHostKeyChecking=accept-new -i CRED.pem CRED.pem azureuser@${IPA1}:/home/azureuser
 scp -i CRED.pem azurevm1/westus3_provision.sh azureuser@${IPA1}:/home/azureuser
-ssh -i CRED.pem azureuser@${IPA1} "scp -o StrictHostKeyChecking=accept-new -i CRED.pem westus3_provision.sh azureuser@${IPD2}:/home/azureuser; ssh -i CRED.pem azureuser@${IPD2} 'source westus3_provision.sh'"
+ssh -i CRED.pem azureuser@${IPA1} "scp -o StrictHostKeyChecking=accept-new -i CRED.pem westus3_provision.sh azureuser@${IPVM}:/home/azureuser; ssh -i CRED.pem azureuser@${IPVM} 'source westus3_provision.sh'"
